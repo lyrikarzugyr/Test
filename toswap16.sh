46c3c6352c927febc4f3417a56cd472116db7e5f
lyrikarzugyr <85294282+lyrikarzugyr@users.noreply.github.com>
2022-03-02 17:13:11 +0800

    Update toswap16.sh

--- toswap16.sh
+++ toswap16.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo -e '\n\e[42m[Swap] Starting...\e[0m\n'
 sudo swapoff -a
-sed -i 's//root/swapfile swap swap defaults 0 0//g' /etc/fstab
 cd $HOME
 sudo fallocate -l 16G $HOME/swapfile
 sudo dd if=/dev/zero of=swapfile bs=1K count=16M
