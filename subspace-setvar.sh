580ca4d1c9807c5c9f5150463299d945306de868
lyrikarzugyr <85294282+lyrikarzugyr@users.noreply.github.com>
2022-06-05 20:02:07 +0800

    Delete subspace-setvar.sh

--- subspace-setvar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ ! $NODE_NAME ]; then
-	read -p "Enter node name: " NODE_NAME
-	echo 'export SUBSPACE_NODENAME='${NODE_NAME} >> $HOME/.bash_profile
-fi
-if [ ! $WALLET_ADDRESS ]; then
-	read -p "Enter wallet address: " WALLET_ADDRESS
-	echo 'export SUBSPACE_WALLET='${WALLET_ADDRESS} >> $HOME/.bash_profile
-fi
