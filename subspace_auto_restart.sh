9d8193cc7084ff57f0a8befb29aae6b2aadd07af
lyrikarzugyr <85294282+lyrikarzugyr@users.noreply.github.com>
2022-06-15 16:50:17 +0800

    Update subspace_auto_restart.sh

--- subspace_auto_restart.sh
+++ subspace_auto_restart.sh
@@ -4,7 +4,7 @@ while(true)
 	do
 #		echo `journalctl -u subspaced -n 1 | grep Waiting`
 		if [[ `journalctl -u subspaced -n 1` =~ "Waiting" ]]; then
-			sudo systemctl restart subspaced-farmer
+			sudo systemctl restart farmerd
 		fi
 		sleep 14
 	done
