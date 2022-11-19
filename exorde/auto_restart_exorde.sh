450026f71b7bcdec1294aa05b5bf34ce871aff4e
lyrikarzugyr <85294282+lyrikarzugyr@users.noreply.github.com>
2022-11-19 10:31:20 +0800

    Create auto_restart_exorde.sh

---
+++ exorde/auto_restart_exorde.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+node_count=`docker ps --filter="name=exorde-cli_" | wc -l`
+if [ ${node_count} = "0" ]; then
+	#echo "无节点"
+else
+	for ((i=1; i<=node_count - 1; i ++))
+	do
+	diyihang=$(docker logs exorde-cli_${i} --tail 4 | sed -n '1p')
+	disanhang=$(docker logs exorde-cli_${i} --tail 4 | sed -n '3p')
+	if [[ $diyihang == "[UPDATE SYSTEM] Checking new updates..." ]];then
+	if [[ $disanhang == "[UPDATE SYSTEM] Checking new updates..." ]];then
+	docker restart exorde-cli_${i}
+	fi
+	fi
+	sleep 1
+	done
+fi
