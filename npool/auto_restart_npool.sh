e0e71d2a2e22987b2aa6e2dcb5bcf2b7efa02b84
lyrikarzugyr <85294282+lyrikarzugyr@users.noreply.github.com>
2022-11-22 08:45:41 +0800

    Update auto_restart_npool.sh

--- npool/auto_restart_npool.sh
+++ npool/auto_restart_npool.sh
@@ -5,7 +5,7 @@ do
   #获取uptime最后一个字段（15分钟负载）
   cpufuzai=$(uptime | awk '{print $NF}')
 
-  # 如果负载>=1.2则重启npool
+  # 如果负载>=1.1则重启npool
   if [ `echo "$cpufuzai>=1.1" |bc` -eq 1 ];then
     systemctl restart npool
   fi
